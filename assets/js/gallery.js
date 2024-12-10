f46ac5a0fb883faf070e14a6b04214121c9ec352
Maxim Dumont <maxim.dumont2000@hotmail.com>
2024-12-11 00:20:38 +0100

    update gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -26,102 +26,102 @@ const artworks = [
     {
         imgUrl: 'Ivy_in_her_prime.JPG',
         title: 'Ivy in her prime',
-        description: 'description'
+        description: 'part of a bedazzled heart universe'
     },
     {
         imgUrl: 'inktober_2024.JPG',
-        title: 'Inktober 2024',
-        description: 'description 2'
+        title: 'Inktober day 11: snacks',
+        description: 'part of inktober 2024\npart of the bedazzled heart universe'
     },
     {
         imgUrl: 'undead_knight.JPG',
         title: 'Undead Knight',
-        description: 'description 2'
+        description: 'A self indulgent persona insert,\nsort of a fictional self portrait as an undead knight since ive been obsessed with medieval\nfantasy lately.'
     },
     {
         imgUrl: 'self_portait.JPG',
         title: 'Self portrait',
-        description: 'description 2'
+        description: 'A self portrait where I showcase my love for gothic fantasy which is a very important genre in\nmy works. Centipedes also appear in some of my works as they are my alter ego, a kind of\nself insert or signature for some of my works when relevant. My birthmark is a source of light\nin this digital piece, i\'d like to imagine it gives me spiritual powers from time to time'
     },
     {
         imgUrl: 'DTIYS_entry_skqpart6k.JPG',
         title: 'DTIYS: skqpart6k',
-        description: 'description 2'
+        description: ''
     },
     {
         imgUrl: 'DTIYS_entry_nlx13k.JPG',
         title: 'DTIYS: nlx13k',
-        description: 'description 2'
+        description: ''
     },
     {
         imgUrl: 'brazillian_miku_trend.JPG',
-        title: 'Brazillian Hatsune Miku',
-        description: 'description 2'
+        title: 'Brazilian Miku trend',
+        description: 'My take on the Brazilian Miku trend that went around the globe this summer of 2024.'
     },
     {
         imgUrl: 'galasynth_participation.JPG',
-        title: 'Galasynth participation',
-        description: 'description 2'
+        title: 'Participation for Galasynth',
+        description: 'My entry for a yearly fanzine called Galasynth, where over 300 artist from around the world\nmake a digital magazine centered around fashion and the concept of Vocaloids which\noriginated from Barcelona and Japan.'
     },
     {
         imgUrl: 'birthday_art_miku.JPG',
         title: 'Birthday art Hatsune Miku',
-        description: 'description 2'
+        description: 'Something small for the infamous Vocaloids birthday and the release of her new figurine.'
     },
     {
         imgUrl: 'lolita_poster.JPG',
-        title: 'Lolita: A bedazzled heart',
-        description: 'description 2'
+        title: 'Lolita; a bedazzled heart Poster',
+        description: 'A potential poster for my story “Lolita; a bedazzled heart”\nYou can see my my main character Lolita staring up at the sky, and Lily the her friend\ntending to her wounds. i tried to convey the core values of the story: friendship, helping other\nand mysteries/puzzles. There\'s a small easter egg in there somewhere as well :)\n\npart of a bedazzled heart universe'
     },
     {
         imgUrl: 'miku_pride_month.JPG',
-        title: 'Hatsune Miku pride month',
-        description: 'description 2'
+        title: 'Miku Ponytail trend X Pride Month',
+        description: 'My take on the Miku ponytail trend in combination with pride month and awareness.'
     },
     {
         imgUrl: 'chibi_madoka.JPG',
         title: 'Chibi Madoka',
-        description: 'description 2'
+        description: 'A chibi version of Madoka from the show ‘puella magi madoka magica\’\nThese were intended to be made merch of: stickers and/or (acrylic) keychains'
     },
     {
         imgUrl: 'chibi_homura.JPG',
         title: 'Chibi Homura',
-        description: 'description 2'
+        description: 'A chibi version of Homura from the show ‘puella magi madoka magica\’\nThese were intended to be made merch of: stickers and/or (acrylic) keychains'
     },
     {
         imgUrl: 'DTIYS_entry_opingpingo.JPG',
         title: 'DTIYS: opingpingo',
-        description: 'description 2'
+        description: ''
     },
     {
         imgUrl: 'art_trade_touchstarved.JPG',
-        title: 'Art trade Touchstarved',
-        description: 'description 2'
+        title: '‘Touchstarved’ Art Trade',
+        description: 'An art trade with another artist in which we drew each others original characters. I was told\nto draw their original character (right) with a fictional character Kuras from a rpg game called\n\‘touchstarved\’.'
     },
     {
         imgUrl: 'the_key_AOT.JPG',
-        title: 'The key from Attack on Titan',
-        description: 'description 2'
+        title: 'The key',
+        description: 'A symbolic piece of fanart for the iconic show of “Attack on Titan”. I illustrated young Eren\nwith the key that will forever change his life and the future of humanity'
     },
     {
         imgUrl: 'Springtime_with_lolita.JPG',
         title: 'Springtime with Lolita',
-        description: 'description 2'
+        description: 'A fresh piece of art of my original character Lolita, but in a different color scheme to get into\nthe spring spirit <3\n\npart of a bedazzled heart universe'
     },
     {
         imgUrl: 'commision_touchstarved.JPG',
         title: 'Commision: Touchstarved',
-        description: 'description 2'
+        description: 'A commission for a dear friend that is obsessed with the new rpg game’ touchstarved’'
     },
     {
         imgUrl: 'DTIYS_entry_sodays10k.JPG',
         title: 'DTIYS: sodays10k',
-        description: 'description 2'
+        description: ''
     },
     {
         imgUrl: 'fall_of_wall_maria_AOT.JPG',
-        title: 'Fall of wall Maria from Attack on Titan 2024',
-        description: 'description 2'
+        title: 'Fall of Wall Maria',
+        description: 'Art of the young trio from the show "Attack on Titan” the moment their wall collapsed\nand their lives changed forever.'
     }
 ]
 const writings = [
